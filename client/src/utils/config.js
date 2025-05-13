5f3539f35aed0b192551cc4b32ed23ca8ee96620
Sumit Gupta <121390884+sumit-gupta7363@users.noreply.github.com>
2025-05-13 16:57:12 +0530

    Update config.js

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://expense-tracker-z8kv.onrender.com/";
+const BASE_URL = "https://expense-tracker-bd6y.onrender.com";
 // const BASE_URL = "http://127.0.0.4:5000";
 
 export { BASE_URL };
